218829505d4b9e5f4de7bf7568fa3e00c48fb954
Andreas Auer <andreas.auer03@gmail.com>
2020-04-27 11:31:58 +0200

    fixed issue when load button pressed multiple times

--- Corona_LiveTime_Diagramm/Form1.cs
+++ Corona_LiveTime_Diagramm/Form1.cs
@@ -12,6 +12,7 @@ namespace Corona_LiveTime_Diagramm
 {
     public partial class Form1 : Form
     {
+        private bool clicked = false;
         public Form1()
         {
             InitializeComponent();
@@ -25,26 +26,54 @@ namespace Corona_LiveTime_Diagramm
 
         private void chart1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void loadDiagramm_Click(object sender, EventArgs e)
         {
-            Webinformation wf = new Webinformation();
-            string[] wfArr = wf.WebInformation();
-            Diagramm dg = new Diagramm();
-            string[] xValues = dg.setDiagramm(wfArr).yValues;
-            string[] yValues = dg.setDiagramm(wfArr).xValues;
-
-            int j = 0;
-            for (int i = 0; i < xValues.Length * 2; i += 2)
+            if (clicked == false)
             {
-                chart1.Series["Death"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i]));
-                chart1.Series["Cases"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i + 1]));
-                chart2.Series["Deaths"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i]));
-                chart3.Series["Cases"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i + 1]));
-                j++;
+                Webinformation wf = new Webinformation();
+                string[] wfArr = wf.WebInformation();
+                Diagramm dg = new Diagramm();
+                string[] xValues = dg.setDiagramm(wfArr).yValues;
+                string[] yValues = dg.setDiagramm(wfArr).xValues;
+
+                int j = 0;
+                for (int i = 0; i < xValues.Length * 2; i += 2)
+                {
+                    chart1.Series["Death"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i]));
+                    chart1.Series["Cases"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i + 1]));
+                    chart2.Series["Deaths"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i]));
+                    chart3.Series["Cases"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i + 1]));
+                    j++;
+                }
+                clicked = true;
+            }
+            else
+            {
+                chart1.Series["Death"].Points.Clear();
+                chart1.Series["Cases"].Points.Clear();
+                chart2.Series["Deaths"].Points.Clear();
+                chart3.Series["Cases"].Points.Clear();
+
+                Webinformation wf = new Webinformation();
+                string[] wfArr = wf.WebInformation();
+                Diagramm dg = new Diagramm();
+                string[] xValues = dg.setDiagramm(wfArr).yValues;
+                string[] yValues = dg.setDiagramm(wfArr).xValues;
+
+                int j = 0;
+                for (int i = 0; i < xValues.Length * 2; i += 2)
+                {
+                    chart1.Series["Death"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i]));
+                    chart1.Series["Cases"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i + 1]));
+                    chart2.Series["Deaths"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i]));
+                    chart3.Series["Cases"].Points.AddXY(xValues[j], Convert.ToInt32(yValues[i + 1]));
+                    j++;
+                }
             }
         }
     }
 }
+
